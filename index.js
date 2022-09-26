b1b5fa0d46e9a4c329e47532b0e56b8d74cdd882
Olexander <cmurtimur@gmail.com>
2022-09-26 19:01:19 +0300

    homeqrk JS 1

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-const f = 10;
-// console.log("Hello!", 5, a)
-// alert('ви впевнені що хочите покинути сторінку?')
-// const shouldRenew = confirm ("бажаєте продовжити підписку?")
-// console.log(shouldRenew)
-// let quantity = prompt(' Ведіть кількість товару')
-// quantity = Number (quantity);
-// console.log(quantity)
-// console.log(typeof quantity)
-// const a = 5;
-// const b = 10;
-// console.log(b % a);
-// const c = 8 ;
-// const t = 3;
-// console.log(c % t);
-// console.log(15 <  3);
-// console.log(15 >  3);
-console.log("10" === "10" );
- const isEqual = 1 !== true
+const str= "Привіт";
+const num= 123;
+const flag= true;
+const txt= "true";
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof flag);
+console.log(typeof txt);
 
- console.log(isEqual);
+const name: "hsafd";
+
+
