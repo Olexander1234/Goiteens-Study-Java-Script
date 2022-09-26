f8e26645e4955ca81edcafb98c10a56d8f000c92
Olexander <cmurtimur@gmail.com>
2022-09-26 16:12:57 +0300

    leson Js 2

---
+++ index.js
@@ -0,0 +1,21 @@
+const f = 10;
+// console.log("Hello!", 5, a)
+// alert('ви впевнені що хочите покинути сторінку?')
+// const shouldRenew = confirm ("бажаєте продовжити підписку?")
+// console.log(shouldRenew)
+// let quantity = prompt(' Ведіть кількість товару')
+// quantity = Number (quantity);
+// console.log(quantity)
+// console.log(typeof quantity)
+// const a = 5;
+// const b = 10;
+// console.log(b % a);
+// const c = 8 ;
+// const t = 3;
+// console.log(c % t);
+// console.log(15 <  3);
+// console.log(15 >  3);
+console.log("10" === "10" );
+ const isEqual = 1 !== true
+
+ console.log(isEqual);
