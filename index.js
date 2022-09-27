b29d473dcbcfb52bb930c5fd1cf88235f833adf5
Olexander <cmurtimur@gmail.com>
2022-09-27 10:09:48 +0300

    ІИ

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const str= "Привіт";
 const num= 123;
 const flag= true;
 const txt= "true";
-console.log(typeof str);
-console.log(typeof num);
-console.log(typeof flag);
-console.log(typeof txt);
+// console.log(typeof str);
+// console.log(typeof num);
+// console.log(typeof flag);
+// console.log(typeof txt);
 
-const name: "hsafd";
+const a = 12;
+const age = 5;
+const number = 98;
+const isAuthenticated = true;
+
+const name = 'Sasha';
 
 
+
+// частина 2
+
+const value = prompt("Напишіть своє ім'я"); 
