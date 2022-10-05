235ca8e1adb871f2d2b52e7657821031749bd631
Olexander <cmurtimur@gmail.com>
2022-10-05 18:00:06 +0300

    Hmewrk 2

--- lhomework-2.js
+++ lhomework-2.js
@@ -1,2 +1,42 @@
-let str = "Hello";
-console.log(Number(str));
+const k1 = Number.parseInt("5px")
+const k2 = Number.parseInt("12.45asdwe12")
+const k3 = Number.parseInt("12djd334")
+const k4 = Number.parseInt("qwqweeewq")
+
+
+const k5 = Number.parseFloat("5px")
+const k6 = Number.parseFloat("12.45asdwe12")
+const k7 = Number.parseFloat("12djd334")
+const k8 = Number.parseFloat("qwqweeewq")
+
+console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18));
+console.log(Math.min(2, 34, 99, 3, 22, 36, 733, 18));
+
+
+console.log(Math.random() * (19 - 3) + 3);
+
+const  result = "5+5+’5’";
+console.log(typeof  result) 
+
+
+const email = "tarnav1514@gmail.com"
+const elementDog = "@"
+console.log(email.includes(elementDog))
+console.log(email.length);
+
+
+
+
+const name = "My"
+const name1 = "name"
+const name2 = "is"
+const fullName = `${name} ${name1} ${name2} Viktor`
+
+console.log(fullName)
+
+
+const userName = "Олексанdр"
+const payment = "300грн "
+alert("Дякуємо, Олександро! До сплати 300 гривень")
+
+
