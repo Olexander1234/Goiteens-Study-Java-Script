ba4a7d93d8710a2db8daddaef42339821afaa1ef
Olexander <cmurtimur@gmail.com>
2022-10-11 15:58:32 +0300

    иячси

---
+++ lesson5.js
@@ -0,0 +1,40 @@
+if( 5 > 8 )
+{
+// тіло
+console.log("hello");
+}
+console.log("hi");
+
+if(
+    100 > 500
+){
+console.log("перша умова вірна");
+
+}
+else{
+    console.log("Перша умова не вірна");
+
+
+
+}
+
+
+
+const salary = 5000   ;
+if( salary > 1000 &&   salary < 2000
+    )
+
+{console.log("Рівень 1")
+
+}
+else if( salary = 2000  && salary < 3000 )
+{console.log("Рівень 2")
+
+}
+else{
+    console.log("hsdtym 3");
+}
+
+
+
+
