3ac1118a018df6f5fb06511e2f8ab90f40920f65
Olexander <cmurtimur@gmail.com>
2022-11-03 16:24:02 +0200

    піча

---
+++ index.js
@@ -0,0 +1,8 @@
+const massage = "Curabitur ligula sapien";
+const maxlenghs = 16;
+if (massage.length <= maxlenghs ) {
+    console.log(massage);
+} else {
+    const newMassage = massage.split('').splice(maxlenghs, )
+
+}
