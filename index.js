3fcbe1e21bcac031adeb782e4322a95a854f9334
Olexander <cmurtimur@gmail.com>
2022-11-08 19:20:38 +0200

    паоа

--- index.js
+++ index.js
@@ -6,3 +6,61 @@ if (massage.length <= maxlenghs ) {
     const newMassage = massage.split('').splice(maxlenghs, )
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Функції  
+
+
+const fn = function (num1, num2) {
+    // тіло
+    return 555;
+   
+  console.log(num1);
+  console.log(num2);
+
+
+  const sum = num1 + num2;
+   console.log("код в функції", sum); 
+
+}
+// fn(10, 20);
+// fn(5, 8 );
+// fn(500, 80 );
+
+// fn(587768679675676844564564659, 63463)
+
+
+console.log('результат функцвї: ', fn(10, 20));
+
+// getTotalPrice()
+// function getTotalPrice() {
+//     console.log("код в функції v564564"); 
+    
+// }
+
+
+
+
+
+
+
