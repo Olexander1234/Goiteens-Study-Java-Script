3e62627c0cf16f497c7900e77c554e5d407941b4
Olexander <cmurtimur@gmail.com>
2022-11-10 18:09:03 +0200

    ваятв

--- index.js
+++ index.js
@@ -1,62 +1,140 @@
-const massage = "Curabitur ligula sapien";
-const maxlenghs = 16;
-if (massage.length <= maxlenghs ) {
-    console.log(massage);
-} else {
-    const newMassage = massage.split('').splice(maxlenghs, )
+// const massage = "Curabitur ligula sapien";
+// const maxlenghs = 16;
+// if (massage.length <= maxlenghs ) {
+//     console.log(massage);
+// } else {
+//     const newMassage = massage.split('').splice(maxlenghs, )
 
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Функції  
-
-
-const fn = function (num1, num2) {
-    // тіло
-    return 555;
-   
-  console.log(num1);
-  console.log(num2);
-
-
-  const sum = num1 + num2;
-   console.log("код в функції", sum); 
-
-}
-// fn(10, 20);
-// fn(5, 8 );
-// fn(500, 80 );
-
-// fn(587768679675676844564564659, 63463)
-
-
-console.log('результат функцвї: ', fn(10, 20));
-
-// getTotalPrice()
-// function getTotalPrice() {
-//     console.log("код в функції v564564"); 
-    
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Функції  
+
+
+// const fn = function (num1, num2) {
+//     // тіло
+//     return 555;
+   
+//   console.log(num1);
+//   console.log(num2);
+
+
+//   const sum = num1 + num2;
+//    console.log("код в функції", sum); 
+
+// }
+// // fn(10, 20);
+// // fn(5, 8 );
+// // fn(500, 80 );
+
+// // fn(587768679675676844564564659, 63463)
+
+
+// console.log('результат функцвї: ', fn(10, 20));
+
+// // getTotalPrice()
+// // function getTotalPrice() {
+// //     console.log("код в функції v564564"); 
+    
+// // }
+
+
+
+
+// /*
+//  * Напиши функцию changeCase(string) яка замінює регістр кожного символа в рядку на протилежний.
+//  * Наприклад, якщо рядок «JavaScript», то на виході повинно бути «jAVAsCRIPT».
+//  */
+
+
+
+
+
+
+// const str = 'JavaScript';
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   /*
+//  * Напиши функцию slugify(string) яка отримує рядок і повертає URL-slug
+//  * Заголовок статті складається тільки з букв та пропусків
+//  */
+// const slugfy = function (str) {
+//     const title = 'Top 10 benefits of React framework';
+// const slugTitle = str.toLocaleLowerCase().split(' ').join('-');
+// returnslugTitle;
+// }
+
+
+
+
+
+
+
+// // console.log(slugTitle);
+  
+//   console.log(slugify('Top 10 benefits of React framework'));
+//   console.log(slugify('Azure Static Web Apps are Awesome'));
+//   console.log(slugify('Technical writing tips for non-native English speakers'));
+
+
+
+
+
+
+
+
+
+
+
 
 
 
